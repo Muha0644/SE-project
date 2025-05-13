d6f3be4aaa12d3c542dac1e44551f5bb5b9e6180
Muha0644 <muhamedmulic9@gmail.com>
2025-05-13 23:33:31 +0200

    add login API

---
+++ se-project/src/app/api/login/route.ts
@@ -0,0 +1,23 @@
+import Database from 'better-sqlite3'
+import bcrypt from 'bcrypt';
+
+export async function POST(request: Request) {
+	let params = await request.json()
+	const db = new Database('database.sqlite') //temp, TODO: use ORM
+	const users = db.prepare('SELECT * FROM users WHERE email = ? AND password = ?').all(
+		params.email,
+		bcrypt.hash(params.password, 1) //1 for performance, higher for prod
+	)
+
+	if(users && users.length == 1){
+		return new Response("Success!", {
+			status: 200
+		})
+		//redirect('/home') idk
+	} else {
+		return new Response("Invalid email or password!",{
+			status: 401
+		})
+	}
+	
+}
